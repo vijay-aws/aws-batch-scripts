be807cd32ef701e90d8bfaa1060953ff5287728b
vijay-aws <46588077+vijay-aws@users.noreply.github.com>
2019-05-25 20:29:49 +0200

    Added seconds logic to the script.

--- checkRunnableBatchJobs.py
+++ checkRunnableBatchJobs.py
@@ -2,6 +2,10 @@
 #!/usr/bin/env python
 import sys
 import boto3
+from pprint import pprint
+import datetime
+import argparse
+
 
 def get_all_jobqueues():
 	""" Get all the Job queues """
@@ -13,25 +17,38 @@ def get_all_jobqueues():
 		myqueues.append(str(queue['jobQueueArn']))
 	return myqueues
 
-def get_all_runnable_jobs():
+def get_all_runnable_jobs(sec):
 	""" Get all the runnable jobs under each queue """
 	client = boto3.client('batch')
 	myjobqueues = get_all_jobqueues()
-	myrunnablejobs = {}
+	allrunnablejobs = []
 	for jobqueue in myjobqueues:
+		myrunnablejob = {}
 		queuejobs = client.list_jobs(jobQueue=jobqueue, jobStatus="RUNNABLE")
 		jobs = queuejobs['jobSummaryList']
-		for job in jobs:
-			myrunnablejobs["jobQueueName"] = jobqueue
-			myrunnablejobs["status"] = job['status']
-			myrunnablejobs["jobId"] = job['jobId']
-	return myrunnablejobs
+		if jobs:
+			for job in jobs:
+				myrunnablejob["jobQueueName"] = jobqueue
+				myrunnablejob["status"] = job['status']
+				myrunnablejob["jobId"] = job['jobId']
+				unixtimestamp = job['createdAt']
+				currenttime = datetime.datetime.now()
+				readable = datetime.datetime.fromtimestamp(unixtimestamp/1000.0)
+				myrunnablejob["JobStuckInSeconds"] = (currenttime - readable).seconds
+				if myrunnablejob["JobStuckInSeconds"] > sec:
+					allrunnablejobs.append(myrunnablejob)
+	return allrunnablejobs
 
 def main():
 	""" Main Function """
-	result = get_all_runnable_jobs()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-s", "--seconds", help="Pass the number of seconds to check the RUNNABLE jobs:", type=int, required=True)
+	args = parser.parse_args()
+
+	result = get_all_runnable_jobs(args.seconds)
 	if result:
-		print("Below are the jobs stuck in RUNNABLE state", result)
+		print("Below are the jobs stuck in RUNNABLE state for more than {0} seconds".format(args.seconds))
+		pprint(result)
 		sys.exit(1)
 	else:
 		sys.exit(0)
